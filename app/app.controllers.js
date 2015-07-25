2d0e3a92e1d0e979ca57aecbc2ae64fd51f546ca
Kit Chio Ng <kcng@ucdavis.edu>
2015-07-24 17:29:21 -0700

    created menu options and their style

--- app/app.controllers.js
+++ app/app.controllers.js
@@ -7,12 +7,21 @@
 
     angular.module('mainCtrl', []);
 
-    angular.module('mainCtrl').controller('indexCtrl',
-        ['$scope', function($scope) {
+    angular.module('mainCtrl').controller('mainCtrl',
+        ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
 
-            $scope.helloWord = "hello";
+            /*
+             * sidenav show/hide function
+             */
+            $scope.toggleNav = function() {
 
+                $mdSidenav('left').toggle();
+            }
+
+            /*
+             * menu options
+             */
+            $scope.menuOptions = ["Home", "Projects", "Resume", "About"];
     }]);
-
 
 }());
