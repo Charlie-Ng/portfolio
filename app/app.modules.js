7c7aa28c3ff2cf7de84fa82636f9743a5de286db
Kit Chio Ng <kcng@ucdavis.edu>
2015-07-27 17:31:42 -0700

    started ngRoute

--- app/app.modules.js
+++ app/app.modules.js
@@ -6,6 +6,18 @@
 
     'use strict';
 
-    angular.module('portfolio', ['ngMaterial', 'mainCtrl']);
+    angular.module('portfolio', ['ngMaterial', 'mainCtrl', 'ngRoute'])
+        .config(['$routeProvider', '$locationProvider', '$compileProvider', function($routeProvider, $locationProvider, $compileProvider) {
 
+            $routeProvider
+                .when('/home', {
+                    templateUrl: '/home/home.html'
+                })
+                .when('/projects', {
+                    templateUrl: '/projects/projects.html'
+                });
+
+            $locationProvider.html5Mode({"enabled": true, "requireBase": false});
+
+        }]);
 }());
