735883a5c5586ee157a0f9b76b1ff55c99872377
Charlie-Ng <kcng@ucdavis.edu>
2015-09-23 18:20:56 -0700

    Update resumeCtrl.js

--- app/templates/resume/resumeCtrl.js
+++ app/templates/resume/resumeCtrl.js
@@ -21,6 +21,42 @@
                     nickName: "Charlie",
                     phone: "408-221-2865",
                     email: "kcng@ucdavis.edu"
+                },
+                
+                objective: "To obtain a job in xxxxx in the field of Computer Science",
+                
+                education: {
+                    
+                    university: "University of California, Davis",
+                    major: "Computer Science, BS",
+                    minor: "none",
+                    GPA: "3.41"
+                    graduateDate: "June 2016"
+                },
+                
+                skills: {
+                    
+                    webDev: "AngularJS"
+                },
+                
+                experiences: {
+                    
+                },
+                
+                projects: {
+                    
+                },
+                
+                leadership: {
+                    
+                },
+                
+                awards: {
+                    
+                },
+                
+                links: {
+                    
                 }
             }
         }]);
