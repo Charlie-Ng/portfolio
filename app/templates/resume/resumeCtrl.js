3e235294733eeb1da4c9d6c971a7463a616bb4f6
Charlie-Ng <kcng@ucdavis.edu>
2015-09-25 00:16:11 -0700

    redesigned resume object and added md-slider to resume html

--- app/templates/resume/resumeCtrl.js
+++ app/templates/resume/resumeCtrl.js
@@ -29,19 +29,102 @@
                 education: {
                     
                     university: "University of California, Davis",
+                    shortUName: "UC Davis",
                     major: "Computer Science, BS",
+                    shortMajor: "CS, BS",
                     minor: "none",
                     GPA: "3.41/4.0",
                     graduateDate: "June 2016"
                 },
                 
                 skills: {
-                    
-                    webDev: "AngularJS"
+
+                    languages: ["Java", "JavaScript", "C", "C++", "HTML5/CSS3", "Git", "AngularJS", "Lunix"],
+                    webLang: ["JavaScript", "AngularJS", "HTML5/CSS3", "MongoDB"],
+                    java: {
+
+                        name: "Java",
+                        rate: 8
+                    },
+
+                    javascript: {
+
+                        name: "JavaScript",
+                        rate: 9
+                    },
+
+                    c: {
+
+                        name: "C",
+                        rate: 8
+                    },
+
+                    cpp: {
+
+                        name: "C++",
+                        rate: 7
+                    },
+
+                    html5css3: {
+
+                        name: "HTML5/CSS3",
+                        rate: 10
+                    },
+
+                    angularjs: {
+
+                        name: "AngularJS",
+                        rate: 9
+                    },
+
+                    git: {
+
+                        name: "Git",
+                        rate: 7
+                    },
+
+                    lunix: {
+
+                        name: "Lunix",
+                        rate: 6
+                    }
                 },
                 
                 experiences: {
-                    
+
+                    job1 : {
+
+                        title: "Applications Developer",
+                        company: "UC Davis",
+                        period: "June 2015 - present",
+                        duties: {
+
+                            duty1: "Designed and implemented UC Davis Aggiefeed, a Twitter like web application streaming campus news, event, and activities",
+                            duty2: "something"
+                        }
+                    },
+
+                    job2 : {
+
+                        title: "Web Designer/Developer Intern",
+                        company: "UC Davis",
+                        period: "March 2015 - June 2015 (3 months)",
+                        duties : {
+
+                            duty1: "Redesigned and implemented UC Davis Physical department website"
+                        }
+                    },
+
+                    job3 : {
+
+                        title: "Math Tutor",
+                        company: "De Anza College",
+                        period: "Sep 2013 - June 2014 (9 months)",
+                        duties : {
+
+                            dutye1: "xxxxxxxxxx xxx xxxxxxxxxxxxx xxx  xxx"
+                        }
+                    }
                 },
                 
                 projects: {
